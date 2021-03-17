e1e5f6542d67af7cd559fcc9e8fc29de499175d8
yuan.xiong <yuan.xiong@intel.com>
2021-03-17 16:59:16 +0800

    Failed to create CPU OpenCL context with OpenCV 4.4.0.
    Signed-off-by: yuan.xiong <yuan.xiong@intel.com>

--- src/opencv-opencl.cpp
+++ src/opencv-opencl.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include "opencv-opencl.hpp"
 
 void helloWorld() {
@@ -32,21 +33,92 @@ void helloWorld() {
 	}
 }
 
-void testOpenCLKernel() {
+void dumpOpenCL() {
 	if(!cv::ocl::haveOpenCL()) {
 		std::cout << "OpenCL is not available, leaving..." << std::endl;
 		return;
 	}
 
-	cv::ocl::Context context = cv::ocl::Context::getDefault();
-	cv::ocl::Device device = context.device(0);
+	std::vector< cv::ocl::PlatformInfo > platform_info;
+	cv::ocl::setUseOpenCL(true);
+	cv::ocl::getPlatfomsInfo(platform_info);
+
+	for (int i = 0; i < platform_info.size(); i++) {
+		cv::ocl::PlatformInfo sdk = platform_info.at(i);
+		for (int j = 0; j < sdk.deviceNumber(); j++) {
+			cv::ocl::Device device;
+			device.TYPE_GPU;
+			sdk.getDevice(device, j);
+
+			std::cout << "\n*********************\n Device " << i + 1 << std::endl;
+			std::cout << "Vendor ID: " << device.vendorID() << std::endl;
+			std::cout << "Vendor name: " << device.vendorName() << std::endl;
+			std::cout << "Name: " << device.name() << std::endl;
+			std::cout << "Driver version: " << device.driverVersion() << std::endl;
+			std::cout << "available: " << device.available() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+
+	std::cout << "[-] GPU context" << std::endl;
+	cv::ocl::Context context;
+	if (!context.create(cv::ocl::Device::TYPE_GPU))
+	{
+		std::cout << "Failed creating the context..." << std::endl;
+		return;
+	}
+	std::cout << context.ndevices() << " GPU devices are detected." << std::endl; //This bit provides an overview of the OpenCL devices you have in your computer
+	for (int i = 0; i < context.ndevices(); i++)
+	{
+		cv::ocl::Device device = context.device(i);
+		std::cout << "name:              " << device.name() << std::endl;
+		std::cout << "available:         " << device.available() << std::endl;
+		std::cout << "imageSupport:      " << device.imageSupport() << std::endl;
+		std::cout << "OpenCL_C_Version:  " << device.OpenCL_C_Version() << std::endl;
+		std::cout << std::endl;
+	}
+
+	std::cout << "[-] CPU context" << std::endl;
+	cv::ocl::Context cpuContext;
+	if (!cpuContext.create(cv::ocl::Device::TYPE_CPU))
+	{
+		std::cout << "Failed creating the context..." << std::endl;
+		return;
+	}
+	std::cout << cpuContext.ndevices() << " CPU devices are detected." << std::endl; //This bit provides an overview of the OpenCL devices you have in your computer
+	for (int i = 0; i < cpuContext.ndevices(); i++)
+	{
+		cv::ocl::Device device = cpuContext.device(i);
+		std::cout << "name:              " << device.name() << std::endl;
+		std::cout << "available:         " << device.available() << std::endl;
+		std::cout << "imageSupport:      " << device.imageSupport() << std::endl;
+		std::cout << "OpenCL_C_Version:  " << device.OpenCL_C_Version() << std::endl;
+		std::cout << std::endl;
+	}
+}
+
+void testOpenCLKernel() {
+	dumpOpenCL();
+
+#if 0
+	//cv::ocl::Context context = cv::ocl::Context::getDefault();
+	//cv::ocl::Device device = context.device(0);
+
+	cv::ocl::Context context;
+	//if(!context.create(cv::ocl::Device::TYPE_CPU)) {
+	if(!context.create(cv::ocl::Device::TYPE_ALL)) {
+		std::cout << "Failed to create context for CPU, leaving..." << std::endl;
+		return;
+	}
 
 	std::cout << "*************************************************" << std::endl;
-	for(int i=0; i<1; ++i) {
+	for(int i=0; i<context.ndevices(); ++i) {
+		cv::ocl::Device device = context.device(i);
 		std::cout << "Name\t" << device.name() << std::endl;
 		std::cout << "OpenCL\t" << device.OpenCL_C_Version() << std::endl;
 		std::cout << std::endl;
 	}
+#endif
 
 	helloWorld();
 }
